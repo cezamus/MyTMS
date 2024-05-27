b331ab1823e1860231e336c8189c64c120253709
cezamus <cezary.m.muszynski@gmail.com>
2024-05-27 22:23:45 +0200

    - disable security config while development is in progress + extend unit tests

--- mytms/src/test/java/pl/cm/mytms/service/impl/TrailerServiceImplTest.java
+++ mytms/src/test/java/pl/cm/mytms/service/impl/TrailerServiceImplTest.java
@@ -6,24 +6,35 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import pl.cm.mytms.model.Trailer;
+import pl.cm.mytms.repository.AssignmentRepository;
 import pl.cm.mytms.repository.TrailerRepository;
 
+import java.util.NoSuchElementException;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.doNothing;
 
 @ExtendWith(MockitoExtension.class)
 class TrailerServiceImplTest {
 
-    private static final String CORRECT_REGISTRATION_ID = "AB12345";
+    private static final String CORRECT_REGISTRATION_ID = "AB00000";
+    private static final String NEW_REGISTRATION_ID = "AB00001";
+    private static final String REGISTRATION_ID_OF_EXISTING_TRAILER = "AB00002";
     private static final String NULL_REGISTRATION_ID = null;
     private static final String BLANK_REGISTRATION_ID = "";
+    private static final Long ID_OF_EXISTING_TRAILER = 1L;
+    private static final Long NEW_ID = 2L;
+    private static final Long NULL_ID = null;
 
     @Mock
     TrailerRepository trailerRepository;
+    @Mock
+    private AssignmentRepository assignmentRepository;
+    @Mock
+    Trailer trailer;
 
     @InjectMocks
     TrailerServiceImpl trailerService;
@@ -39,19 +50,104 @@ class TrailerServiceImplTest {
     }
 
     @Test
+    void shouldUpdateTrailerByIdIfDataIsCorrect() {
+        // given
+        when(trailerRepository.existsById(ID_OF_EXISTING_TRAILER)).thenReturn(true);
+        when(trailerRepository.findTrailerByRegistrationId(NEW_REGISTRATION_ID)).thenReturn(Optional.empty());
+        when(trailerRepository.getReferenceById(ID_OF_EXISTING_TRAILER)).thenReturn(trailer);
+        // when
+        trailerService.update(ID_OF_EXISTING_TRAILER, NEW_REGISTRATION_ID);
+        // then
+        verify(trailer).setRegistrationId(NEW_REGISTRATION_ID);
+    }
+
+    @Test
+    void shouldUpdateVehicleByRegistrationIdIfDataIsCorrect() {
+        // given
+        when(trailerRepository.findTrailerByRegistrationId(NEW_REGISTRATION_ID))
+                .thenReturn(Optional.empty());
+        when(trailerRepository.findTrailerByRegistrationId(REGISTRATION_ID_OF_EXISTING_TRAILER))
+                .thenReturn(Optional.of(trailer));
+        // when
+        trailerService.update(REGISTRATION_ID_OF_EXISTING_TRAILER, NEW_REGISTRATION_ID);
+        // then
+        verify(trailer).setRegistrationId(NEW_REGISTRATION_ID);
+    }
+
+    @Test
     void shouldThrowExceptionWhenRegistrationIdIsNull() {
-        assertThrows(IllegalArgumentException.class, () -> trailerService.create(NULL_REGISTRATION_ID));
+        assertThrows(IllegalArgumentException.class,
+                () -> trailerService.create(NULL_REGISTRATION_ID));
+        assertThrows(IllegalArgumentException.class,
+                () -> trailerService.update(ID_OF_EXISTING_TRAILER, NULL_REGISTRATION_ID));
+        assertThrows(IllegalArgumentException.class,
+                () -> trailerService.update(REGISTRATION_ID_OF_EXISTING_TRAILER, NULL_REGISTRATION_ID));
     }
 
     @Test
     void shouldThrowExceptionWhenRegistrationIdIsBlank() {
-        assertThrows(IllegalArgumentException.class, () -> trailerService.create(BLANK_REGISTRATION_ID));
-    }
+        assertThrows(IllegalArgumentException.class,
+                () -> trailerService.create(BLANK_REGISTRATION_ID));
+        assertThrows(IllegalArgumentException.class,
+                () -> trailerService.update(ID_OF_EXISTING_TRAILER, BLANK_REGISTRATION_ID));
+        assertThrows(IllegalArgumentException.class,
+                () -> trailerService.update(REGISTRATION_ID_OF_EXISTING_TRAILER, BLANK_REGISTRATION_ID));    }
 
     @Test
     void shouldThrowExceptionWhenRegistrationIdIsDuplicated() {
-        when(trailerRepository.findTrailerByRegistrationId(CORRECT_REGISTRATION_ID))
+        when(trailerRepository.findTrailerByRegistrationId(REGISTRATION_ID_OF_EXISTING_TRAILER))
                 .thenReturn(Optional.of(new Trailer()));
-        assertThrows(IllegalStateException.class, () -> trailerService.create(CORRECT_REGISTRATION_ID));
+        when(trailerRepository.existsById(ID_OF_EXISTING_TRAILER))
+                .thenReturn(true);
+
+        assertThrows(IllegalStateException.class,
+                () -> trailerService.create(REGISTRATION_ID_OF_EXISTING_TRAILER));
+        assertThrows(IllegalStateException.class,
+                () -> trailerService.update(ID_OF_EXISTING_TRAILER, REGISTRATION_ID_OF_EXISTING_TRAILER));
+        assertThrows(IllegalStateException.class,
+                () -> trailerService.update(REGISTRATION_ID_OF_EXISTING_TRAILER, REGISTRATION_ID_OF_EXISTING_TRAILER));
+    }
+
+    @Test
+    void shouldDeleteVehicleByIdIfDataIsCorrect(){
+        // given
+        when(trailerRepository.existsById(ID_OF_EXISTING_TRAILER)).thenReturn(true);
+        doNothing().when(assignmentRepository).deleteAllByTrailer(any());
+        // when
+        trailerService.delete(ID_OF_EXISTING_TRAILER);
+        // then
+        verify(trailerRepository).deleteById(any());
+    }
+
+    @Test
+    void shouldDeleteVehicleByRegistrationIdIfDataIsCorrect(){
+        // given
+        when(trailerRepository.findTrailerByRegistrationId(REGISTRATION_ID_OF_EXISTING_TRAILER))
+                .thenReturn(Optional.of(trailer));
+        doNothing().when(assignmentRepository).deleteAllByTrailer(trailer);
+        // when
+        trailerService.delete(REGISTRATION_ID_OF_EXISTING_TRAILER);
+        // then
+        verify(trailerRepository).delete(trailer);
+    }
+
+    @Test
+    void shouldThrowExceptionWhenIdIsUnknown() {
+        assertThrows(NoSuchElementException.class,
+                () -> trailerService.update(NULL_ID, CORRECT_REGISTRATION_ID));
+        assertThrows(NoSuchElementException.class,
+                () -> trailerService.update(NEW_ID, CORRECT_REGISTRATION_ID));
+        assertThrows(NoSuchElementException.class,
+                () -> trailerService.update(NULL_REGISTRATION_ID, CORRECT_REGISTRATION_ID));
+        assertThrows(NoSuchElementException.class,
+                () -> trailerService.update(NEW_REGISTRATION_ID, CORRECT_REGISTRATION_ID));
+        assertThrows(NoSuchElementException.class,
+                () -> trailerService.delete(NULL_ID));
+        assertThrows(NoSuchElementException.class,
+                () -> trailerService.delete(NEW_ID));
+        assertThrows(NoSuchElementException.class,
+                () -> trailerService.delete(NULL_REGISTRATION_ID));
+        assertThrows(NoSuchElementException.class,
+                () -> trailerService.delete(NEW_REGISTRATION_ID));
     }
 }
--- mytms/src/test/java/pl/cm/mytms/service/impl/VehicleServiceImplTest.java
+++ mytms/src/test/java/pl/cm/mytms/service/impl/VehicleServiceImplTest.java
@@ -6,8 +6,10 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import pl.cm.mytms.model.Vehicle;
+import pl.cm.mytms.repository.AssignmentRepository;
 import pl.cm.mytms.repository.VehicleRepository;
 
+import java.util.NoSuchElementException;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -16,14 +18,19 @@ import static org.mockito.Mockito.*;
 @ExtendWith(MockitoExtension.class)
 class VehicleServiceImplTest {
 
-    private static final String CORRECT_REGISTRATION_ID = "AB12345";
-    private static final String REPLACEMENT_REGISTRATION_ID = "AB67890";
+    private static final String CORRECT_REGISTRATION_ID = "AB00000";
+    private static final String NEW_REGISTRATION_ID = "AB00001";
+    private static final String REGISTRATION_ID_OF_EXISTING_VEHICLE = "AB00002";
     private static final String NULL_REGISTRATION_ID = null;
     private static final String BLANK_REGISTRATION_ID = "";
-    private static final Long ID = 1L;
+    private static final Long ID_OF_EXISTING_VEHICLE = 1L;
+    private static final Long NEW_ID = 2L;
+    private static final Long NULL_ID = null;
 
     @Mock
     private VehicleRepository vehicleRepository;
+    @Mock
+    private AssignmentRepository assignmentRepository;
     @Mock
     private Vehicle vehicle;
 
@@ -41,31 +48,105 @@ class VehicleServiceImplTest {
     }
 
     @Test
-    void shouldThrowExceptionWhenRegistrationIdIsNull() {
-        assertThrows(IllegalArgumentException.class, () -> vehicleService.create(NULL_REGISTRATION_ID));
+    void shouldUpdateVehicleByIdIfDataIsCorrect() {
+        // given
+        when(vehicleRepository.existsById(ID_OF_EXISTING_VEHICLE)).thenReturn(true);
+        when(vehicleRepository.findVehicleByRegistrationId(NEW_REGISTRATION_ID)).thenReturn(Optional.empty());
+        when(vehicleRepository.getReferenceById(ID_OF_EXISTING_VEHICLE)).thenReturn(vehicle);
+        // when
+        vehicleService.update(ID_OF_EXISTING_VEHICLE, NEW_REGISTRATION_ID);
+        // then
+        verify(vehicle).setRegistrationId(NEW_REGISTRATION_ID);
     }
 
     @Test
-    void shouldThrowExceptionWhenRegistrationIdIsBlank() {
-        assertThrows(IllegalArgumentException.class, () -> vehicleService.create(BLANK_REGISTRATION_ID));
+    void shouldUpdateVehicleByRegistrationIdIfDataIsCorrect() {
+        // given
+        when(vehicleRepository.findVehicleByRegistrationId(NEW_REGISTRATION_ID))
+                .thenReturn(Optional.empty());
+        when(vehicleRepository.findVehicleByRegistrationId(REGISTRATION_ID_OF_EXISTING_VEHICLE))
+                .thenReturn(Optional.of(vehicle));
+        // when
+        vehicleService.update(REGISTRATION_ID_OF_EXISTING_VEHICLE, NEW_REGISTRATION_ID);
+        // then
+        verify(vehicle).setRegistrationId(NEW_REGISTRATION_ID);
+    }
+
+    @Test
+    void shouldThrowExceptionWhenNewRegistrationIdIsNull() {
+        assertThrows(IllegalArgumentException.class,
+                () -> vehicleService.create(NULL_REGISTRATION_ID));
+        assertThrows(IllegalArgumentException.class,
+                () -> vehicleService.update(ID_OF_EXISTING_VEHICLE, NULL_REGISTRATION_ID));
+        assertThrows(IllegalArgumentException.class,
+                () -> vehicleService.update(REGISTRATION_ID_OF_EXISTING_VEHICLE, NULL_REGISTRATION_ID));
+    }
+
+    @Test
+    void shouldThrowExceptionWhenNewRegistrationIdIsBlank() {
+        assertThrows(IllegalArgumentException.class,
+                () -> vehicleService.create(BLANK_REGISTRATION_ID));
+        assertThrows(IllegalArgumentException.class,
+                () -> vehicleService.update(ID_OF_EXISTING_VEHICLE, BLANK_REGISTRATION_ID));
+        assertThrows(IllegalArgumentException.class,
+                () -> vehicleService.update(REGISTRATION_ID_OF_EXISTING_VEHICLE, BLANK_REGISTRATION_ID));
     }
 
     @Test
     void shouldThrowExceptionWhenRegistrationIdIsDuplicated() {
-        when(vehicleRepository.findVehicleByRegistrationId(CORRECT_REGISTRATION_ID))
+        when(vehicleRepository.findVehicleByRegistrationId(REGISTRATION_ID_OF_EXISTING_VEHICLE))
                 .thenReturn(Optional.of(new Vehicle()));
-        assertThrows(IllegalStateException.class, () -> vehicleService.create(CORRECT_REGISTRATION_ID));
+        when(vehicleRepository.existsById(ID_OF_EXISTING_VEHICLE))
+                .thenReturn(true);
+
+        assertThrows(IllegalStateException.class,
+                () -> vehicleService.create(REGISTRATION_ID_OF_EXISTING_VEHICLE));
+        assertThrows(IllegalStateException.class,
+                () -> vehicleService.update(ID_OF_EXISTING_VEHICLE, REGISTRATION_ID_OF_EXISTING_VEHICLE));
+        assertThrows(IllegalStateException.class,
+                () -> vehicleService.update(REGISTRATION_ID_OF_EXISTING_VEHICLE, REGISTRATION_ID_OF_EXISTING_VEHICLE));
     }
 
     @Test
-    void shouldUpdateVehicleByIdIfDataIsCorrect() {
+    void shouldDeleteVehicleByIdIfDataIsCorrect(){
         // given
-        when(vehicleRepository.existsById(ID)).thenReturn(true);
-        when(vehicleRepository.findVehicleByRegistrationId(REPLACEMENT_REGISTRATION_ID)).thenReturn(Optional.empty());
-        when(vehicleRepository.getReferenceById(ID)).thenReturn(vehicle);
+        when(vehicleRepository.existsById(ID_OF_EXISTING_VEHICLE)).thenReturn(true);
+        doNothing().when(assignmentRepository).deleteAllByVehicle(any());
         // when
-        vehicleService.update(ID, REPLACEMENT_REGISTRATION_ID);
+        vehicleService.delete(ID_OF_EXISTING_VEHICLE);
         // then
-        verify(vehicle).setRegistrationId(REPLACEMENT_REGISTRATION_ID);
+        verify(vehicleRepository).deleteById(any());
+    }
+
+    @Test
+    void shouldDeleteVehicleByRegistrationIdIfDataIsCorrect(){
+        // given
+        when(vehicleRepository.findVehicleByRegistrationId(REGISTRATION_ID_OF_EXISTING_VEHICLE))
+                .thenReturn(Optional.of(vehicle));
+        doNothing().when(assignmentRepository).deleteAllByVehicle(vehicle);
+        // when
+        vehicleService.delete(REGISTRATION_ID_OF_EXISTING_VEHICLE);
+        // then
+        verify(vehicleRepository).delete(vehicle);
+    }
+
+    @Test
+    void shouldThrowExceptionWhenIdIsUnknown() {
+        assertThrows(NoSuchElementException.class,
+                () -> vehicleService.update(NULL_ID, CORRECT_REGISTRATION_ID));
+        assertThrows(NoSuchElementException.class,
+                () -> vehicleService.update(NEW_ID, CORRECT_REGISTRATION_ID));
+        assertThrows(NoSuchElementException.class,
+                () -> vehicleService.update(NULL_REGISTRATION_ID, CORRECT_REGISTRATION_ID));
+        assertThrows(NoSuchElementException.class,
+                () -> vehicleService.update(NEW_REGISTRATION_ID, CORRECT_REGISTRATION_ID));
+        assertThrows(NoSuchElementException.class,
+                () -> vehicleService.delete(NULL_ID));
+        assertThrows(NoSuchElementException.class,
+                () -> vehicleService.delete(NEW_ID));
+        assertThrows(NoSuchElementException.class,
+                () -> vehicleService.delete(NULL_REGISTRATION_ID));
+        assertThrows(NoSuchElementException.class,
+                () -> vehicleService.delete(NEW_REGISTRATION_ID));
     }
 }
